3266e42dd1e925f3b106cf26435be367c70a8ce4
Tanu-priya02 <121554388+Tanu-priya02@users.noreply.github.com>
2024-09-27 13:58:02 +0530

    Update Eth-avax4.sol

--- Eth-avax4.sol
+++ Eth-avax4.sol
@@ -4,54 +4,54 @@ pragma solidity ^0.8.18;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract AnnuToken is ERC20, Ownable {
     struct Item {
         string name;
         uint256 cost;
     }
 
-    Item[] public redeemableItems;
-    mapping(address => mapping(uint256 => bool)) public itemRedeemed;
+    Item[] public annuRedeemableItems;
+    mapping(address => mapping(uint256 => bool)) public annuItemRedeemed;
 
-    constructor() ERC20("Degen", "DGN") {
-        initializeRedeemableItems();
+    constructor() ERC20("Annu", "ANNU") {
+        initializeAnnuRedeemableItems();
     }
 
-    function initializeRedeemableItems() internal {
-        redeemableItems.push(Item("Item 1", 100));
-        redeemableItems.push(Item("Item 2", 200));
-        redeemableItems.push(Item("Item 3", 300));
-        redeemableItems.push(Item("Item 4", 400));
+    function initializeAnnuRedeemableItems() internal {
+        annuRedeemableItems.push(Item("Annu Item 1", 100));
+        annuRedeemableItems.push(Item("Annu Item 2", 200));
+        annuRedeemableItems.push(Item("Annu Item 3", 300));
+        annuRedeemableItems.push(Item("Annu Item 4", 400));
     }
 
-    function mintToken(address to, uint256 amount) external onlyOwner {
+    function mintAnnuToken(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 
-    function burnToken(uint256 amount) external {
+    function burnAnnuToken(uint256 amount) external {
         _burn(msg.sender, amount);
     }
 
-    function transferTokens(address to, uint256 amount) external onlyOwner {
+    function transferAnnuTokens(address to, uint256 amount) external onlyOwner {
         _transfer(msg.sender, to, amount);
     }
 
-    function redeem(uint256 itemId) external {
-        require(itemId < redeemableItems.length, "Item does not exist");
-        require(balanceOf(msg.sender) >= redeemableItems[itemId].cost, "Insufficient balance");
-        require(!itemRedeemed[msg.sender][itemId], "Item already redeemed");
+    function redeemAnnuItem(uint256 itemId) external {
+        require(itemId < annuRedeemableItems.length, "Annu Item does not exist");
+        require(balanceOf(msg.sender) >= annuRedeemableItems[itemId].cost, "Insufficient Annu balance");
+        require(!annuItemRedeemed[msg.sender][itemId], "Annu Item already redeemed");
 
-        // Burn tokens as payment for the redeemed item
-        _burn(msg.sender, redeemableItems[itemId].cost);
-        
-        // Mark item as redeemed for the user
-        itemRedeemed[msg.sender][itemId] = true;
+        // Burn Annu tokens as payment for the redeemed item
+        _burn(msg.sender, annuRedeemableItems[itemId].cost);
+
+        // Mark Annu item as redeemed for the user
+        annuItemRedeemed[msg.sender][itemId] = true;
     }
 
-    function getRedeemedItems(address user) external view returns (bool[] memory) {
-        bool[] memory redeemed = new bool[](redeemableItems.length);
-        for (uint256 i = 0; i < redeemableItems.length; i++) {
-            redeemed[i] = itemRedeemed[user][i];
+    function getAnnuRedeemedItems(address user) external view returns (bool[] memory) {
+        bool[] memory redeemed = new bool[](annuRedeemableItems.length);
+        for (uint256 i = 0; i < annuRedeemableItems.length; i++) {
+            redeemed[i] = annuItemRedeemed[user][i];
         }
         return redeemed;
     }
